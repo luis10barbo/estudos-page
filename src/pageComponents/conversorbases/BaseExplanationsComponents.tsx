f34a0beb6280ef5fcd16cbb05bf050db7e51a99d
luis10barbo <luis10barbo2020@gmail.com>
2023-03-25 16:40:15 -0300

    Base Explanation button components

--- src/pageComponents/conversorbases/BaseExplanationsComponents.tsx
+++ src/pageComponents/conversorbases/BaseExplanationsComponents.tsx
@@ -1,133 +1,136 @@
+import { useRef, useState } from "react";
+import { reverseState } from "~/utils/react";
 import { NumberStringComponent } from "../NumberStringComponent";
 import BaseComparisonComponent from "./BaseComparisonComponent";
 
 export const BinaryExplanationComponent: React.FC = () => {
+  const [isFocused, setFocused] = useState(true);
+
+  const explanationRef = useRef<HTMLDivElement>(null);
+
+  function getExplanationHeight() {
+    if (explanationRef.current !== null)
+      return explanationRef.current.clientHeight;
+  }
+
   return (
-    <div className="binary-explanation mb-4 flex w-full flex-col items-center gap-4 rounded-xl bg-[rgb(90,90,90)] p-4 text-center text-white">
-      <p className="w-full border-b border-white pb-4 font-bold">
-        Explicação Binario:
-      </p>{" "}
-      <br />
-      Cada quadradinho no binário, <br />
-      diferente do decimal, que pode conter algarísmos entre 0-9,
-      <br />
-      somente pode conter dois números, 0 ou 1
-      <BaseComparisonComponent
-        numberStrings={["0", "1"]}
-        firstBaseName="decimal"
-        secondBaseName="binary"
-      />
-      <div className="flex items-center gap-2">
-        mas para o
-        <NumberStringComponent baseName={"decimal"} numberString="2" /> é
-        necessario adicionar +1 quadradinho:
+    <div
+      className={`binary-explanation mb-4 flex w-full flex-col items-center rounded-xl bg-gradient-to-tr from-blue-800 to-purple-600 text-center text-white  duration-75 ${
+        isFocused ? "" : "hover:from-blue-600 hover:to-purple-400"
+      }`}
+    >
+      <div
+        className="w-full cursor-pointer select-none border-white p-4 font-bold"
+        onClick={() => {
+          reverseState(setFocused);
+        }}
+      >
+        Explicação Binario
       </div>
-      <BaseComparisonComponent
-        numberStrings={["0", "1", "2", "3"]}
-        firstBaseName="decimal"
-        secondBaseName="binary"
-      />
-      já que o binário só permite números de 0 a 1.
+      <div
+        style={{
+          height: isFocused ? getExplanationHeight() + "px" : "0",
+        }}
+        className={`content  w-full overflow-hidden px-4 duration-75 ${
+          isFocused ? "h-fit overflow-auto border-t" : "h-0"
+        }`}
+      >
+        <div
+          ref={explanationRef}
+          id="binary-explanation"
+          className="flex flex-col gap-2  p-4"
+        >
+          <br />
+          Cada quadradinho no binário, <br />
+          diferente do decimal, que pode conter algarísmos entre 0-9,
+          <br />
+          somente pode conter dois números, 0 ou 1
+          <BaseComparisonComponent
+            numberStrings={["0", "1"]}
+            firstBaseName="decimal"
+            secondBaseName="binary"
+          />
+          <div className="flex items-center justify-center gap-2">
+            mas para o
+            <NumberStringComponent baseName={"decimal"} numberString="2" /> é
+            necessario adicionar +1 quadradinho:
+          </div>
+          <BaseComparisonComponent
+            numberStrings={["0", "1", "2", "3"]}
+            firstBaseName="decimal"
+            secondBaseName="binary"
+          />
+          já que o binário só permite números de 0 a 1.
+        </div>
+      </div>
     </div>
   );
 };
 
 export const HexadecimalExplanationComponent: React.FC = () => {
+  const [isFocused, setFocused] = useState(true);
+
+  const explanationRef = useRef<HTMLDivElement>(null);
+
+  function getExplanationHeight() {
+    if (explanationRef.current !== null)
+      return explanationRef.current.clientHeight;
+  }
+
   return (
-    <div className="explanation mb-4 flex w-full flex-col items-center gap-4 border-b p-4 text-center">
-      <p className="font-bold">Explicação Hexadecimal:</p> <br />
-      Cada quadradinho no hexadecimal suporta, <br />
-      como no decimal, algorismos entre 0-9 (hex: 0-9).
-      <br />
-      Contudo, ele vai além: suportando também o 10-15 (hex: a-f)
-      {["a", "b", "c", "d", "e", "f"].map((element, index) => {
-        return (
+    <>
+      <div
+        // style={{
+        //   background:
+        //     "linear-gradient(to top right, rgb(56, 0, 255), rgb(141, 109, 255))",
+        // }}
+        className="binary-explanation   mb-4 flex w-full flex-col  items-center rounded-xl bg-gradient-to-tr from-blue-800  to-purple-600 text-center text-white"
+      >
+        <div
+          className="w-full cursor-pointer select-none border-white p-4 font-bold"
+          onClick={() => {
+            reverseState(setFocused);
+          }}
+        >
+          Explicação Hexadecimal
+        </div>
+        <div
+          style={{
+            height: isFocused ? "0" : getExplanationHeight() + "px",
+          }}
+          className={`content  w-full overflow-hidden px-4 duration-75 ${
+            isFocused ? "h-0" : "h-fit border-t"
+          }`}
+        >
           <div
-            className="flex items-center gap-2 rounded-lg bg-black/80 p-2 text-white"
-            key={index}
+            ref={explanationRef}
+            id="binary-explanation"
+            className="flex flex-col gap-2 p-4"
           >
-            <div className="binary-number rounded-lg  border border-[rgba(0,0,0,0.5)] py-2 px-4">
-              {10 + index}
+            Cada quadradinho no hexadecimal suporta,
+            <br /> como no decimal, algorismos entre 0-9 (hex: 0-9).
+            <br /> Contudo, ele vai além: suportando também o 10-15 (hex: a-f)
+            <BaseComparisonComponent
+              numberStrings={["10", "11", "12", "13", "14", "15"]}
+              firstBaseName="decimal"
+              secondBaseName="hexadecimal"
+            />
+            <div className="flex items-center justify-center gap-2">
+              ao ultrapassar o{" "}
+              <NumberStringComponent
+                baseName={"hexadecimal"}
+                numberString="f"
+              />{" "}
+              é é necessario adicionar +1 quadradinho:
             </div>
-            (decimal) que é equivalente ao
-            <div className="binary-number rounded-lg  border border-[rgba(0,0,0,0.5)] py-2 px-4">
-              {element}
-            </div>
-            (hexadecimal), <br />
+            <BaseComparisonComponent
+              numberStrings={["15", "16", "31", "32"]}
+              firstBaseName="decimal"
+              secondBaseName="hexadecimal"
+            />
           </div>
-        );
-      })}
-      <div className="flex items-center gap-2">
-        ao ultrapassar o
-        <div className="binary-number rounded-lg  border border-[rgba(0,0,0,0.5)] py-2 px-4">
-          f
         </div>
-        (hexadecimal) é necessario adicionar +1 quadradinho:
       </div>
-      <div className="flex items-center gap-2 rounded-lg bg-black/80 p-2 text-white">
-        <div className="binary-number rounded-lg  border border-[rgba(0,0,0,0.5)] py-2 px-4">
-          1
-        </div>
-        <div className="binary-number rounded-lg  border border-[rgba(0,0,0,0.5)] py-2 px-4">
-          5
-        </div>
-        (decimal) é igual a
-        <div className="binary-number rounded-lg  border border-[rgba(0,0,0,0.5)] py-2 px-4">
-          F
-        </div>
-        (hexadecimal)
-      </div>
-      <div className="flex items-center gap-2 rounded-lg bg-black/80 p-2 text-white">
-        <div className="binary-number rounded-lg  border border-[rgba(0,0,0,0.5)] py-2 px-4">
-          1
-        </div>
-        <div className="binary-number rounded-lg  border border-[rgba(0,0,0,0.5)] py-2 px-4">
-          6
-        </div>
-        (decimal) é igual a
-        <div className="binary-number rounded-lg  border border-[rgba(0,0,0,0.5)] py-2 px-4">
-          1
-        </div>
-        <div className="binary-number rounded-lg  border border-[rgba(0,0,0,0.5)] py-2 px-4">
-          0
-        </div>
-        (hexadecimal)
-      </div>
-      <div className="flex items-center gap-2 rounded-lg bg-black/80 p-2 text-white">
-        <div className="binary-number rounded-lg  border border-[rgba(0,0,0,0.5)] py-2 px-4">
-          3
-        </div>
-        <div className="binary-number rounded-lg  border border-[rgba(0,0,0,0.5)] py-2 px-4">
-          1
-        </div>
-        (decimal) é igual a
-        <div className="binary-number rounded-lg  border border-[rgba(0,0,0,0.5)] py-2 px-4">
-          1
-        </div>
-        <div className="binary-number rounded-lg  border border-[rgba(0,0,0,0.5)] py-2 px-4">
-          F
-        </div>
-        (hexadecimal)
-      </div>
-      <div className="flex items-center gap-2 rounded-lg bg-black/80 p-2 text-white">
-        <div className="binary-number rounded-lg  border border-[rgba(0,0,0,0.5)] py-2 px-4">
-          3
-        </div>
-        <div className="binary-number rounded-lg  border border-[rgba(0,0,0,0.5)] py-2 px-4">
-          2
-        </div>
-        (decimal) é igual a
-        <div className="binary-number rounded-lg  border border-[rgba(0,0,0,0.5)] py-2 px-4">
-          2
-        </div>
-        <div className="binary-number rounded-lg  border border-[rgba(0,0,0,0.5)] py-2 px-4">
-          0
-        </div>
-        <div className="binary-number rounded-lg  border border-[rgba(0,0,0,0.5)] py-2 px-4">
-          0
-        </div>
-        (hexadecimal)
-      </div>
-    </div>
+    </>
   );
 };
